8a3838a65840343720b8ff1bf9ac0dff1979ba06
Danver Braganza <danver@imbue.com>
2025-04-07 18:07:17 -0700

    Add jj to my cargo installs

--- cargo-installs.sh
+++ cargo-installs.sh
@@ -3,3 +3,5 @@ cargo install ripgrep
 cargo install bat
 cargo install hexyl
 cargo install --git https://github.com/astral-sh/uv uv
+cargo install cargo-binstall
+cargo binstall --strategies crate-meta-data jj-cli
